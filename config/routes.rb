4d41ba8e69b0536f4d1165837d8377338afc350e
庄进 <zhuangjin@szkingdom.com>
2022-08-18 16:57:47 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post "/users", to: "users#create"
-  get "/users/:id", to: "users#show"
+  namespace :api do
+    namespace :v1 do
+       #/api/v1
+       resources :validation_codes, only: [:create]
+       # 发送验证码
+       # 只保留创建功能
+       resource :session, only: [:create, :destroy]
+       # 登入/登出
+       resource :me, only:[:show]
+       # 当前用户
+       resources :items
+       # 记账数据
+       resources :tags
+       #创建标签
+    end
+  end
 end
